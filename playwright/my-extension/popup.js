f14276993dfceaf29be328914e4b0726d8933298
hkimura <hiroyasuk@4510.work>
2026-03-23 16:42:42 +0900

    Clear all existing content

--- playwright/my-extension/popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('open-btn').addEventListener('click', () => {
-  chrome.tabs.create({ url: 'https://playwright.dev' });
-});
-
-document.getElementById('close-btn').addEventListener('click', () => {
-  window.close();
-});
--- webhook/gas/main.py
+++
@@ -1,6 +0,0 @@
-import functions_framework
-
-@functions_framework.http
-def hello_get(request):
-    name = request.args.get("name", "World")
-    return f"Hello, {name}!"
